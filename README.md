2acab9af66edbfc028261751c5a4ef00a37afde5
Your Name <you@example.com>
2024-03-06 22:35:29 +0800

    all

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # guessing_game
+
+# 运行
+
+```shell
+cargo run
+```
+
+# 打包
+
+```shell
+cargo build
+```
+
+# 生产打包
+
+```shell
+cargo build --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数字游戏");
+
+    let secret_number = thread_rng().gen_range(1..101);
+    println!("神秘的数字： {}", secret_number);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+    
+        let guess:u32 = match  guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue 
+        };
+    
+        println!("你猜测的数是： {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("相等, YOU WIN!");
+                break;
+            }
+        }
+    }
 }
